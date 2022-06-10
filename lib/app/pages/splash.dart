e126c121c527b2f1734666205f263e2af9ea5172
Everton Lima <inovacaointeligentes@gmail.com>
2022-06-10 18:44:03 -0300

    tela de splash.dart

---
+++ lib/app/pages/splash.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'login_page.dart';
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({Key? key}) : super(key: key);
+
+  @override
+  _SplashPageState createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(
+      Duration(seconds: 10),
+    ).then(
+      (value) => Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (BuildContext context) => LoginPage(),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      child: Container(
+        alignment: Alignment.center,
+        child: SingleChildScrollView(
+          child: Container(
+            child: Column(
+              children: [
+                Center(
+                  child: Lottie.network(
+                    //'https://assets4.lottiefiles.com/packages/lf20_syqnfe7c.json',
+                    'https://assets8.lottiefiles.com/packages/lf20_65mlytpx.json',
+                    repeat: true,
+                    animate: true,
+                    alignment: Alignment.center,
+                  ),
+                ),
+                Text(
+                  'Estudo de Revisão flutter Everton Lima',
+                  style: TextStyle(fontSize: 20.0),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
