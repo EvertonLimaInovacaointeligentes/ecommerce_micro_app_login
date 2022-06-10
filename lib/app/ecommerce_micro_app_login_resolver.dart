66efee8b8e32a75f16f19c897a6330f73ab8ddb4
Everton Lima <inovacaointeligentes@gmail.com>
2022-06-10 18:02:46 -0300

    Ajuste do nome do arquivo login resolver

--- lib/app/ecommerce_micro_app_login_resolver.dart
+++ lib/app/ecommerce_micro_app_login_resolver.dart
@@ -3,7 +3,7 @@ import 'package:ecommerce_micro_core/ecommerce_micro_core.dart';
 
 import 'pages/login_page.dart';
 
-class MicroAppLoginResolver implements MicroApp {
+class EcommerceMicroAppLoginResolver implements MicroApp {
   @override
   String get microAppName => 'micro_app_login';
 
